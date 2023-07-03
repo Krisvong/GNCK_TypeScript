b5b9d1bac5d64bc0093d8eaa28ce6056ee55e0bd
Kristen von Gerichten <kristenvongerichten@kristens-mbp.lan>
2023-07-03 12:00:50 -0400

    updated server side to ts

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pool = void 0;
 // This file sets up a PostgreSQL database connection pool using the 'pg' library.
-const pg_1 = require("pg");
+import { Pool } from 'pg';
 // Load environment variables from a .env file if it exists.
 require('dotenv').config();
+
 // Import the Pool class from the 'pg' library.
 // A pool is a collection of reusable database connections that can be shared across multiple requests, allowing for more efficient and scalable database interactions
 const { Pool: PgPool } = require('pg');
+
 // Create a new Pool object using the PG_URI environment variable.
-const pool = new pg_1.Pool({
-    connectionString: process.env.PG_URI,
+const pool = new Pool({
+  connectionString: process.env.PG_URI,
 });
-exports.pool = pool;
+
+// Export the pool object so it can be used by other modules.
+export { pool };
